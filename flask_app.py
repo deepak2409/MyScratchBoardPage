0b7945403a643d5bf29e6a66c1e71398999d4253
deepak2409 <61558056+deepak2409@users.noreply.github.com>
2020-04-29 01:22:38 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -20,7 +20,7 @@ class Comment(db.Model):
 
     __tablename__ = "comments"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id =  db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
 
